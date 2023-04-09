05800b60bf1ef518161731dc2be176924a619100
toum <nanhtavadh@gmail.com>
2023-04-09 11:27:47 +0700

    build: fix android  minSdkVersion to 19

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 import 'package:tutors/core/error/exceptions.dart';
 
@@ -52,24 +53,23 @@ class AuthService {
       throw ServerException(e.toString());
     }
   }
-  // Future<void> signInWithGoogle() async {
-  //   // Trigger the authentication flow
-  //   final GoogleSignInAccount? googleUser =
-  //       await GoogleSignIn(scopes: <String>["email"]).signIn();
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(scopes: <String>["email"]).signIn();
 
-  //   // Obtain the auth details from the request
-  //   final GoogleSignInAuthentication googleAuth =
-  //       await googleUser!.authentication;
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
 
-  //   // Create a new credential
-  //   final credential = GoogleAuthProvider.credential(
-  //     accessToken: googleAuth.accessToken,
-  //     idToken: googleAuth.idToken,
-  //   );
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
 
-  //   // Once signed in, return the UserCredential
-  //   await FirebaseAuth.instance.signInWithCredential(credential);
-  // }
+    // Once signed in, return the UserCredential
+    final user = await _auth.signInWithCredential(credential);
+    return user;
+  }
 
   Future<void> signOut() async {
     await _auth.signOut();
